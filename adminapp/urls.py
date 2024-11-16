2c5d2cd0dbbf7ebf1ed5d33b6bd616b6bf288df0
KASHYAPCT <kashyapchandran38@gmail.com>
2024-11-16 16:42:47 +0530

    add adminapp

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     
-    path('', dashboard, name='dashboard')
+    path('', dashboard, name='dashboard'),
+    path('imageadd/',image_add,name='imageadd')
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -5,3 +5,29 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/admin/login/')  
 def dashboard(Request):
     return render(Request,'admin/dashboard.html')
+
+@login_required(login_url='/admin/login/')
+def image_add(request):
+    current_page = 'imageadd'
+    # if request.method == 'POST':
+    #     image = request.FILES.get('image') 
+    #     head = request.POST.get('head')
+    #     description = request.POST.get('description')
+        
+    #     try:
+    #         door = Door(
+    #             image=image,
+    #             head=head,
+    #             description=description,
+    #         )
+    #         door.save()
+    #         messages.success(request, 'door added successfully')
+    #         return redirect('door_list')
+    #     except Exception as e:
+    #         messages.error(request, f'Error adding door: {e}')
+    #         return redirect('door_add')
+        
+    context = {
+        'current_page': current_page,
+    } 
+    return render(request, 'admin/imageadd.html', context)
